ca9f9b49c8d4901a1fb528cf355458a193fcd2eb
Reid Matthews <rmatthews@corelogic.com>
2020-01-10 16:18:09 -0500

    End of Section 17

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -26,7 +26,6 @@ export class MemberMessagesComponent implements OnInit {
     this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
       .pipe(
         tap(messages => {
-          debugger;
           for (let i = 0; i < messages.length; i++) {
             if (messages[i].isRead === false && messages[i].recipientId === currentUserId) {
               this.userService.markAsRead(currentUserId, messages[i].id);
